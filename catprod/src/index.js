8f92aaf81449bba4e4671839096d2a94b30476af
Alexander Kalskov <Alexander.Kalskov@arcadia.spb.ru>
2018-02-08 13:39:56 +0300

    changed catprod

--- catprod/src/index.js
+++ catprod/src/index.js
@@ -17,45 +17,12 @@ var producer = new rdkafka.Producer({
   'dr_cb': true,
 });
 console.log('Starting...');
-
-// Connect to the broker manually
 producer.connect();
-
-// Wait for the ready event before proceeding
-producer.on('ready', () => {
-  console.log('Ready for sending...');
-  try {
-    producer.produce(
-      // Topic to send the message to
-      'topic',
-      // optionally we can manually specify a partition for the message
-      // this defaults to -1 - which will use librdkafka's default partitioner (consistent random for keyed messages, random for unkeyed messages)
-      null,
-      // Message to send. Must be a buffer
-      new Buffer('Awesome message333'),
-    );
-    console.log('Message has been sent...');
-  } catch (err) {
-    console.error('A problem occurred when sending our message');
-    console.error(err);
-  }
-});
-
-// Any errors we encounter, including connection errors
-producer.on('event.error', (err) => {
-  console.error('Error from producer');
-  console.error(err);
-})
-
-
-// --- Consumer ---
 
 var consumer = new rdkafka.KafkaConsumer({
   'group.id': 'kafka',
   'metadata.broker.list': 'kafka:9092',
 }, {});
-
-// Non-flowing mode
 consumer.connect();
 
 
@@ -73,7 +40,37 @@ consumer
 
     consul.kv.get('redis/test', function(err, result) {
       if (err) throw err;
-      console.log(result);
+
+        try {
+          producer.produce(
+            // Topic to send the message to
+            'topic',
+            // optionally we can manually specify a partition for the message
+            // this defaults to -1 - which will use librdkafka's default partitioner (consistent random for keyed messages, random for unkeyed messages)
+            null,
+            // Message to send. Must be a buffer
+            new Buffer(result.Value),
+          );
+          console.log('Message has been sent...');
+        } catch (err) {
+          console.error('A problem occurred when sending our message');
+          console.error(err);
+        }
+
+      console.log('+++', result.Value);
     });    
   });
+
+// Wait for the ready event before proceeding
+
+
+// Any errors we encounter, including connection errors
+producer.on('event.error', (err) => {
+  console.error('Error from producer');
+  console.error(err);
+})
+
+
+// --- Consumer ---
+
 
